d959a500ad4873d99f58470b4faa2b8907444091
Miguel Grado <mgradob@gmail.com>
2018-10-31 13:47:18 -0700

    Day 1

--- app/src/main/java/com/test/mgb/tecchat/MainActivity.kt
+++ app/src/main/java/com/test/mgb/tecchat/MainActivity.kt
@@ -2,11 +2,28 @@ package com.test.mgb.tecchat
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.RecyclerView
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+
+        val editText = findViewById<EditText>(R.id.editText)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            Toast.makeText(this, editText.text.toString(), Toast.LENGTH_LONG).show()
+        }
     }
 }
